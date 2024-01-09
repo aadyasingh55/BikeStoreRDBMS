74d6d2b11e1193347aceaf9a449f2d7cb51651f8
Aadya Singh <76063220+aadyasingh55@users.noreply.github.com>
2024-01-10 03:22:13 +0530

    Update BrazillianEComm.sql

--- BrazillianEComm.sql
+++ BrazillianEComm.sql
@@ -1,3 +1,4 @@
+--Customer Loyalty Analysis
 SELECT o.customer_id, c.first_name,c.last_name,
 COUNT(o.order_id) AS tot_orders,
 ROUND(SUM(i.list_price - (i.list_price*i.discount)),2) AS Lifetime_val,
@@ -9,10 +10,25 @@ JOIN products p ON p.product_id = i.product_id
 GROUP BY o.customer_id
 ORDER BY Lifetime_val DESC
 LIMIT 10;
+
+--Staff Performance Metrics
 SELECT s.staff_id,CONCAT(s.first_name,' ',s.last_name) AS staff_name,AVG(DATEDIFF(o.shipped_date,o.order_date)) AS Avg_Processing_time
 FROM staffs s JOIN orders o ON o.staff_id=s.staff_id
 GROUP BY s.staff_id
 ORDER BY Avg_Processing_time ASC;
+
+--Inventory Turnover Analysis
+SELECT c.category_name,
+  COUNT(DISTINCT s.product_id) AS distinct_products,
+  SUM(s.quantity) AS total_quantity_sold,
+  SUM(s.quantity) / COUNT(DISTINCT s.product_id) AS turnover_rate
+FROM stocks s
+JOIN products p ON s.product_id = p.product_id
+JOIN categories c ON p.category_id = c.category_id
+GROUP BY c.category_name
+ORDER BY turnover_rate DESC;
+
+-- Seasonal Sales Analysis
 SELECT EXTRACT(MONTH FROM o.order_date) AS month,
 EXTRACT(YEAR FROM o.order_date) AS year,
 ROUND(SUM(p.list_price-(p.list_price*i.discount)),2) AS Monthly_Revenue
