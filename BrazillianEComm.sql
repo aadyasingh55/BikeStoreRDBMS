d11a6e250b87c5145e58951d59d89cfda7e525b0
Aadya Singh <76063220+aadyasingh55@users.noreply.github.com>
2024-01-14 00:18:58 +0530

    Update BrazillianEComm.sql

--- BrazillianEComm.sql
+++ BrazillianEComm.sql
@@ -1,4 +1,5 @@
---Customer Loyalty Analysis
+USE bike_store;
+-- Identified customers who have made the most purchases and calculated their lifetime value
 SELECT o.customer_id, c.first_name,c.last_name,
 COUNT(o.order_id) AS tot_orders,
 ROUND(SUM(i.list_price - (i.list_price*i.discount)),2) AS Lifetime_val,
@@ -10,25 +11,12 @@ JOIN products p ON p.product_id = i.product_id
 GROUP BY o.customer_id
 ORDER BY Lifetime_val DESC
 LIMIT 10;
-
---Staff Performance Metrics
+-- Evaluate the performance of staff by calculating the average order processing time (time taken to ship an order) for each staff member
 SELECT s.staff_id,CONCAT(s.first_name,' ',s.last_name) AS staff_name,AVG(DATEDIFF(o.shipped_date,o.order_date)) AS Avg_Processing_time
 FROM staffs s JOIN orders o ON o.staff_id=s.staff_id
 GROUP BY s.staff_id
 ORDER BY Avg_Processing_time ASC;
-
---Inventory Turnover Analysis
-SELECT c.category_name,
-  COUNT(DISTINCT s.product_id) AS distinct_products,
-  SUM(s.quantity) AS total_quantity_sold,
-  SUM(s.quantity) / COUNT(DISTINCT s.product_id) AS turnover_rate
-FROM stocks s
-JOIN products p ON s.product_id = p.product_id
-JOIN categories c ON p.category_id = c.category_id
-GROUP BY c.category_name
-ORDER BY turnover_rate DESC;
-
--- Seasonal Sales Analysis
+-- Analyze the seasonality of sales by calculating the monthly revenue and comparing it year-over-year
 SELECT EXTRACT(MONTH FROM o.order_date) AS month,
 EXTRACT(YEAR FROM o.order_date) AS year,
 ROUND(SUM(p.list_price-(p.list_price*i.discount)),2) AS Monthly_Revenue
@@ -37,6 +25,23 @@ JOIN order_items i ON o.order_id=i.order_id
 JOIN products p ON p.product_id=i.product_id
 GROUP BY month,year
 ORDER BY year;
+-- Determine the inventory turnover rate for each product category to optimize stock levels.
+SELECT c.category_name,COUNT(DISTINCT s.product_id) AS distinct_products,SUM(s.quantity) AS total_quantity_sold,
+SUM(s.quantity) / COUNT(DISTINCT s.product_id) AS turnover_rate
+FROM stocks s
+JOIN products p ON s.product_id = p.product_id
+JOIN categories c ON p.category_id = c.category_id
+GROUP BY c.category_name
+ORDER BY turnover_rate DESC;
+-- Determine the inventory turnover rate for each product category to optimize stock levels.
+SELECT c.category_name,COUNT(DISTINCT s.product_id) AS distinct_products,SUM(s.quantity) AS total_quantity_sold,
+    SUM(s.quantity) / COUNT(DISTINCT s.product_id) AS turnover_rate
+FROM stocks s
+JOIN products p ON s.product_id = p.product_id
+JOIN categories c ON p.category_id = c.category_id
+GROUP BY c.category_name
+ORDER BY turnover_rate DESC;
+
 
 
 
